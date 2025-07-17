934136352609c6cb1bf716b9f4eb0c43b7fa33fe
Yasin Kereci <yke144907@stud.gibb.ch>
2025-07-17 10:10:17 +0200

    chore(entitylayer): install Entity Framework Core and related packages

--- ProjectSensive.PresentationLayer/obj/Debug/net8.0/ProjectSensive.PresentationLayer.AssemblyInfo.cs
+++ ProjectSensive.PresentationLayer/obj/Debug/net8.0/ProjectSensive.PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSensive.PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0423e9408c9cc116ce599841a6da79adbf05e754")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11598f370fb6413adc422e8b3e4175d81d5fbbe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSensive.PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSensive.PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
