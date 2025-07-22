b04040741b2d113d2f0e6eba88ed1a72458fabd2
Yasin Kereci <yke144907@stud.gibb.ch>
2025-07-22 18:40:06 +0200

    ...

--- ProjectSensive.DataAccessLayer/obj/Debug/net8.0/ProjectSensive.DataAccessLayer.AssemblyInfo.cs
+++ ProjectSensive.DataAccessLayer/obj/Debug/net8.0/ProjectSensive.DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSensive.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63f740e58ca2efa19092998535e6c07264bea74b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d56dd3e9aa543ace69de769b54cf032c1f58ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSensive.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSensive.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
